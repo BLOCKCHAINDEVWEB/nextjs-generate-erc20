cd341a996135db663c95841c1aa1edb1b767dae7
httpcode <blockchaindevweb@gmail.com>
2022-01-05 10:21:41 +0100

    add network switch

--- pages/index.js
+++ pages/index.js
@@ -7,22 +7,20 @@ import useWeb3 from '../lib/useWeb3'
 import Erc20Mock from '../smart-contract-abi/ERC20Mock.json'
 import { Button } from '../components/Button'
 import { SelectNetwork } from '../components/SelectNetwork'
+import { NETWORKS_LIST, CHAINS_ID_TO_NETWORKS_LIST } from '../utils/networksList'
 
-
-export default function Home({ status, tokensErc20 }) {
+export default function Home({ tokensErc20 }) {
   const router = useRouter()
 
   const {
-    provider,
-    signer,
     chainIdUsed,
     accounts,
-    balance,
     connectWallet
   } = useWeb3()
 
   const [isConnectedWeb3, setIsConnectedWeb3] = useState(false)
   const [networkSelected, setNetworkSelected] = useState([])
+  const [networkNameUsed, setNetworkNameUsed] = useState(null)
   const [tokenName, setTokenName] = useState('ERC20Mock')
   const [tokenSymbol, setTokenSymbol] = useState('ERC20M')
   const [totalSupplyEth, setTotalSupplyEth] = useState('1000')
@@ -37,18 +35,6 @@ export default function Home({ status, tokensErc20 }) {
   const NEXT_PUBLIC_MUMBAI_PROVIDER_URL = process.env.NEXT_PUBLIC_MUMBAI_PROVIDER_URL
   const NEXT_PUBLIC_HOST_URL = process.env.NEXT_PUBLIC_HOST_URL
 
-  const BLOCKCHAIN_EXPLORER_URL_MAP = {
-    goerli: 'https://goerli.etherscan.io',
-    sokol: 'https://blockscout.com/poa/sokol',
-    mumbai: 'https://mumbai.polygonscan.com'
-  };
-
-  const CHAINS_ID_TO_NETWORKS_LIST = {
-    5: 'goerli',
-    77: 'sokol',
-    80001: 'mumbai'
-  }
-
   // refresh props!
   const refreshData = () => {
     router.replace(router.asPath);
@@ -60,8 +46,8 @@ export default function Home({ status, tokensErc20 }) {
       setIsDisabledButton(false)
     }
 
-    const networkNameUsed = CHAINS_ID_TO_NETWORKS_LIST[parseInt(chainIdUsed)] || ''
-    if (networkNameUsed === networkSelected) setIsDisabledButton(false)
+    setNetworkNameUsed(CHAINS_ID_TO_NETWORKS_LIST[parseInt(chainIdUsed)] || '')
+    if (networkNameUsed) setIsDisabledButton(false)
   }, [accounts, chainIdUsed])
 
   const createToken = async token => {
@@ -74,6 +60,22 @@ export default function Home({ status, tokensErc20 }) {
     })
 
     return res
+  }
+
+  const handleNetworkSwitch = async () => {
+    if (!NETWORKS_LIST[networkSelected]?.chainId) return
+
+    try {
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: NETWORKS_LIST[networkSelected].chainId }]
+      })
+
+    } catch (error) {
+      console.error(`Error request wallet_switchEthereumChain method.`)
+    }
+
+    connectWallet()
   }
 
   const handleGenerateErc20 = async () => {
@@ -177,12 +179,20 @@ export default function Home({ status, tokensErc20 }) {
               />
             </div>
           </div>
-          <Button
-            className="inline-flex justify-center w-[200px] rounded-md border border-transparent shadow-sm px-10 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
-            action={handleGenerateErc20}
-            content={isLoading ? (<><svg className="animate-spin -ml-6 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>Generate Erc20</>) : "Submit"}
-            isDisabled={isDisabledButton}
-          />
+          {networkNameUsed === networkSelected
+            ? <Button
+                className="inline-flex justify-center w-[250px] rounded-md border border-transparent shadow-sm px-10 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
+                action={handleGenerateErc20}
+                content={isLoading ? (<><svg className="animate-spin -ml-6 mr-2 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle><path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path></svg>Generate Erc20</>) : "Submit"}
+                isDisabled={isDisabledButton}
+              />
+            : <Button
+                className="inline-flex justify-center w-[250px] rounded-md border border-transparent shadow-sm px-10 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
+                action={handleNetworkSwitch}
+                content={`Switch network ${networkSelected}`}
+                isDisabled={isDisabledButton}
+              />
+          }
         </div>
         <div className="p-5">
           <div className="overflow-x-auto">
@@ -210,7 +220,7 @@ export default function Home({ status, tokensErc20 }) {
                       <div className="flex items-center">
                         <div className="flex-1 font-medium text-gray-800">
                           <a
-                            href={`${BLOCKCHAIN_EXPLORER_URL_MAP[token.token_network]}/address/${token.contract_address}`}
+                            href={`${NETWORKS_LIST[token.token_network].blockExplorerUrls}/address/${token.contract_address}`}
                             className="underline text-blue-800 mr-3"
                             target="_blank"
                             rel="noreferrer"
